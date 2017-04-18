8d4b45549b53c8d11b05903d2bb7ed9b3b9b4b7f
Nolan Marketti <nolan@futuremandigital.com>
2017-04-18 14:15:47 -0500

    Added Staging CircleCI
    Updated Config with correct Staging CircleCI

--- config.rb
+++ config.rb
@@ -49,6 +49,12 @@ configure :development do
  activate :livereload
 end
 
+#CircleCI Staging
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                = 'staging.enemcreative.com'
+  s3_sync.region                = 'us-east-1'
+end
+
 configure :build do
   activate :minify_css
   activate :minify_javascript
--- environments/staging.rb
+++
@@ -1,5 +0,0 @@
-#CircleCI Staging
-activate :s3_sync do |s3_sync|
-  s3_sync.bucket                = 'staging.enemcreative.com'
-  s3_sync.region                = 'us-east-1'
-end
