df35e79e2b694dd4e943e465c432ca0e566f207b
Nolan Marketti <nolan@futuremandigital.com>
2017-04-18 01:54:46 -0500

    Setup CircleCI
    Initial CircleCI Staging Setup

--- config.rb
+++ config.rb
@@ -56,3 +56,9 @@ end
 
 # Portfolio
 page "portfolio/*", :layout => "portfolio"
+
+#CircleCI Staging
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                = 'staging.enemcreative.com'
+  s3_sync.region                = 'us-east-1'
+end
