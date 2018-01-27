6ceff0f4d32bdbf43d8d3a552d3bd25abe8adc4d
Nolan Marketti <nolan@enemcreative.com>
2018-01-27 13:55:17 -0600

    Added Production CircleCI

--- config.rb
+++ config.rb
@@ -45,9 +45,21 @@ activate :directory_indexes
 
 
 #CircleCI Staging
+configure :staging do
 activate :s3_sync do |s3_sync|
   s3_sync.bucket                = 'staging.enemcreative.com'
   s3_sync.region                = 'us-east-1'
+  end
+end
+
+
+configure :production do
+    activate :s3_sync do |s3_sync|
+      s3_sync.bucket                = 'enemcreative.com'
+      s3_sync.region                = 'us-east-1'
+      default_caching_policy max_age: (60 * 60 * 24 * 365)
+      caching_policy 'text/html', public: true, max_age: 0, must_revalidate: true
+   end
 end
 
 
